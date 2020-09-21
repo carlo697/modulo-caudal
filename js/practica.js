64735f8253fa38ec0dc367d1f1236d9b5d4e8d24
Carlos Peña <carlos.07888@gmail.com>
2020-09-21 19:30:18 -0400

    Arreglar fecha en practicas

--- js/practica.js
+++ js/practica.js
@@ -8,4 +8,19 @@ for (input of preguntas) {
 	}
 }
 
+const fecha = document.querySelector("#fecha");
+const date = new Date();
+
+const dia = date.getDate();
+let mes = date.getMonth() + 1;
+if (mes < 10) {
+	mes = "0" + mes;
+}
+const year = date.getFullYear();
+
+fecha.textContent = `${dia}-${mes}-${year}`;
+
+
 print();
+
+console.log(date)
