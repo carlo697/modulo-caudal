f1060870e645e6282459c9dce3a0d27825d72e3e
Carlos Peña <carlos.07888@gmail.com>
2020-11-13 11:17:31 -0400

    Actualizar imprenta de las practicas: añadir logo del IUTVAL, limpiar codigo javascript, agregar campos nuevos

--- js/practica.js
+++ js/practica.js
@@ -1,26 +1,30 @@
 const preguntas = document.querySelectorAll("[preguntaId]");
+const fecha = document.querySelector("#fecha");
 
-for (input of preguntas) {
-	const valor = localStorage.getItem(input.getAttribute("preguntaId"));
+cargarDatos();
+actualizarFecha();
 
-	if (valor != null) {
-		input.textContent = valor;
+function cargarDatos() {
+	for (input of preguntas) {
+		const valor = localStorage.getItem(input.getAttribute("preguntaId"));
+
+		if (valor != null) {
+			input.textContent = valor;
+		}
 	}
 }
 
-const fecha = document.querySelector("#fecha");
-const date = new Date();
+function actualizarFecha() {
+	const date = new Date();
 
-const dia = date.getDate();
-let mes = date.getMonth() + 1;
-if (mes < 10) {
-	mes = "0" + mes;
+	const dia = date.getDate();
+	let mes = date.getMonth() + 1;
+	if (mes < 10) {
+		mes = "0" + mes;
+	}
+	const year = date.getFullYear();
+	
+	fecha.textContent = `${dia}-${mes}-${year}`;
 }
-const year = date.getFullYear();
-
-fecha.textContent = `${dia}-${mes}-${year}`;
-
 
 print();
-
-console.log(date)
